4eb66409d4e24335123e0b11674621ae83a95298
mlovse1 <67603844+mlovse1@users.noreply.github.com>
2021-01-31 20:09:41 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Coffee loading...\n')
+   response.end('Coffee loading...Send More Coffee\n')
 }).listen(3333)
 
 // Console will print the message
